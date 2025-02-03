f93d14c85cd76c9f137bbabb495219480ba16a4b
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2025-02-03 19:43:14 +0200

    parsing

---
+++ app/Main.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import           Codec.Text.IConv
+import qualified Data.ByteString           as BS
+import qualified Data.ByteString.Lazy.UTF8 as LBS
+import           Data.Char
+import           Data.List
+import           Data.List.Split
+import           System.IO
+
+main :: IO ()
+main = do
+  -- Read the file as a ByteString
+  bs <- (dropWhile (not . isAlpha) . LBS.toString . convert "CP1251" "UTF-8" . BS.fromStrict) <$> BS.readFile "name.key"
+  let (n : d' : _) = splitOn "_" bs
+  let d = intercalate "/" $ splitOn "-" d'
+  putStrLn $ "Name: " <> n
+  putStrLn $ "Date: " <> d
+  writeFile "output.csv" (n <> "," <> d)
