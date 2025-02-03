6dba83ef797da94a786c9827a88a04e8ddef033d
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2025-02-03 20:32:12 +0200

    minor fixes

--- app/Main.hs
+++ app/Main.hs
@@ -13,13 +13,16 @@ import           System.Directory
 main :: IO ()
 main = do
   -- Read the file as a ByteString
-  bs <- (dropWhile (not . isAlpha) . LBS.toString . convert "CP1251" "UTF-8" . BS.fromStrict) <$> BS.readFile "name.key"
-  let (n : d' : _) = splitOn "_" bs
-  let d = intercalate "/" $ splitOn "-" d'
-  putStrLn $ "Name: " <> n
-  putStrLn $ "Date: " <> d
-  writeFile "output.csv" (n <> "," <> d)
   dirs <- listDirectory "."
-  files <- findFiles dirs "file1"
-  mapM_ putStrLn files
-  pure ()
+  files <- findFiles dirs "name.key"
+  list <- traverse convertFile files
+  let content = intercalate "\n" list
+  writeFile "output.csv" content
+  putStrLn "Done!"
+
+convertFile :: FilePath -> IO String
+convertFile file = do
+  content <- (dropWhile (not . isAlpha) . LBS.toString . convert "CP1251" "UTF-8" . BS.fromStrict) <$> BS.readFile file
+  let (n : d' : _) = splitOn "_" content
+  let d = intercalate "/" $ splitOn "-" d'
+  pure (n <> "," <> d)
